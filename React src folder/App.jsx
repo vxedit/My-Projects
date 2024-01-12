eaf7c8a3d36620159f974b93fe49e8e27e6fef2c
Irfan__s <75405825+vxedit@users.noreply.github.com>
2024-01-12 18:54:25 +0530

    Add files via upload

---
+++ React src folder/App.jsx
@@ -0,0 +1,21 @@
+// import StateObj from "./components/Obj"
+// import State from "./components/State"
+// import Fetch from "./components/Fetchdata"
+// import Ftab from "./components/Fetchtab"
+import Axi from "./components/Fetchaxios"
+// import Fetdata from "./components/getbyid"
+const App=()=>
+{
+    return(<div>
+        {/* <State></State> */}
+        {/* <StateObj/> */}
+        {/* <p>---------fetch------</p> */}
+        {/* <Fetch/> */}
+        {/* <p>---------table--------</p> */}
+        {/* <Ftab/> */}
+        <Axi></Axi>
+        {/* <Fetdata></Fetdata> */}
+        </div>
+    )
+}
+export default App
